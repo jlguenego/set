df9c4c60bba5db30727a9e0d3a2f75a48aa9b83d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-16 19:49:14 +0100

    doc: added doc

--- src/Sets.ts
+++ src/Sets.ts
@@ -1,9 +1,27 @@
 export class Sets {
+  /**
+   * add all elements of source to target
+   *
+   * @static
+   * @template T
+   * @param {Set<T>} target
+   * @param {Set<T>} source
+   * @memberof Sets
+   */
   static absorb<T>(target: Set<T>, source: Set<T>): void {
     for (const e of source) {
       target.add(e);
     }
   }
+  /**
+   * Builds a new set = a ∪ b
+   *
+   * @static
+   * @template T
+   * @param {...Set<T>[]} sets
+   * @returns {Set<T>}
+   * @memberof Sets
+   */
   static union<T>(...sets: Set<T>[]): Set<T> {
     const result = new Set<T>();
     for (const set of sets) {
@@ -11,6 +29,16 @@ export class Sets {
     }
     return result;
   }
+
+  /**
+   * Builds a new set = a ∩ b
+   *
+   * @static
+   * @template T
+   * @param {...Set<T>[]} sets
+   * @returns {Set<T>}
+   * @memberof Sets
+   */
   static intersection<T>(...sets: Set<T>[]): Set<T> {
     const result = new Set<T>();
     for (const e of sets[0]) {
@@ -28,6 +56,16 @@ export class Sets {
     return result;
   }
 
+  /**
+   * Build the set of elements of b that are not element of a.
+   *
+   * @static
+   * @template T
+   * @param {Set<T>} a
+   * @param {Set<T>} b
+   * @returns {Set<T>}
+   * @memberof Sets
+   */
   static complementary<T>(a: Set<T>, b: Set<T>): Set<T> {
     const result = new Set<T>();
     for (const e of b) {
@@ -38,6 +76,16 @@ export class Sets {
     return result;
   }
 
+  /**
+   * Tests if a = b (a ⊂ b and b ⊂ a)
+   *
+   * @static
+   * @template T
+   * @param {Set<T>} a
+   * @param {Set<T>} b
+   * @returns {boolean}
+   * @memberof Sets
+   */
   static areEquals<T>(a: Set<T>, b: Set<T>): boolean {
     if (a.size !== b.size) {
       return false;
