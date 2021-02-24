86b413619725c5ab874aefb53b444e1635cf5a96
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-24 10:50:40 +0100

    feat: improved toIdString for set
    Added sha3.

--- src/Memoizer.ts
+++ src/Memoizer.ts
@@ -1,3 +1,13 @@
+import {SHA3} from 'sha3';
+
+// const hash = new SHA3(256);
+
+const hash = (str: string) => {
+  const sha3 = new SHA3(512);
+  sha3.update(str);
+  return sha3.digest('hex');
+};
+
 /**
  * Try to generate a unique id according the content of an object.
  *
@@ -7,21 +17,21 @@
 export const toIdString = (o: Object): string => {
   if (typeof o === 'symbol') {
     // warning: different symbols can have the same toString() result.
-    return 'symbol_' + o.toString();
+    return hash('symbol_' + o.toString());
   }
   if (typeof o !== 'object') {
-    return typeof o + '_' + o;
+    return hash(typeof o + '_' + o);
   }
   if (o instanceof Set) {
-    return 'Set' + toIdString([...o]);
+    return hash('Set' + toIdString([...o]));
   }
-  return (
+  return hash(
     o.constructor.name +
-    '{' +
-    Object.keys(o)
-      .map(key => key + '=' + toIdString((o as {[key: string]: Object})[key]))
-      .join(',') +
-    '}'
+      '{' +
+      Object.keys(o)
+        .map(key => key + '=' + toIdString((o as {[key: string]: Object})[key]))
+        .join(',') +
+      '}'
   );
 };
 
