49bffe1bf8dd438362e2cee4e5f998e4ef9f64b1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-25 19:09:29 +0100

    feat: topological sort

---
+++ src/relations/TopologicalSort.ts
@@ -0,0 +1,27 @@
+import {RelationOn} from './RelationOn';
+export const topologicalSort = <T>(
+  partialOrder: RelationOn<T>,
+  domain: Set<T>
+): RelationOn<T> => {
+  if (!partialOrder.isPartialOrder(domain)) {
+    throw new Error('Cannot do a topological sort on non partial order');
+  }
+  const ai = new Set(domain);
+  const result: T[] = [];
+  while (ai.size > 0) {
+    const minA = [...ai].find(a => {
+      for (const b of ai) {
+        if (partialOrder.test(b, a)) {
+          return false;
+        }
+      }
+      return true;
+    });
+    if (minA === undefined) {
+      throw new Error('you should not come here');
+    }
+    ai.delete(minA);
+    result.push(minA);
+  }
+  return new RelationOn<T>((a, b) => result.indexOf(a) < result.indexOf(b));
+};
--- test/order.spec.ts
+++ test/order.spec.ts
@@ -1,10 +1,20 @@
+import {OrderedPair} from './../src/OrderedPair';
 import assert from 'assert';
 import {RelationOn} from '../src';
+import {topologicalSort} from '../src/relations/TopologicalSort';
 
 describe('Order Unit Test', () => {
   it('test partialOrder', () => {
     const domain = new Set([1, 2, 3]);
     const lessThan = new RelationOn<number>((a, b) => a < b);
     assert.deepStrictEqual(lessThan.isPartialOrder(domain), true);
+  });
+
+  it('test topologicalOrder', () => {
+    const domain = new Set([1, 2, 3, 0]);
+    const partial = RelationOn.fromSet(new Set([new OrderedPair(1, 2)]));
+    const linear = topologicalSort(partial, domain);
+    const result = linear.isLinearOrder(domain);
+    assert.deepStrictEqual(result, true);
   });
 });
